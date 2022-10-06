d6dcbcc9c65f6a938ecefedfd6fcefead8216b8a
drestharx <huascarhfe2014@gmail.com>
2022-10-06 19:01:55 -0400

    Se concluyo el modulo de descuentos - metodos find y filter

--- math/percents.js
+++ math/percents.js
@@ -25,3 +25,73 @@ function calculoDescuento() {
 
     p.innerText = 'El precio con descuento sera de ' + precioDescontado + '$';
 }
+
+// Cupones
+
+const cuponPrice = document.getElementById('precio');
+const cuponDiscount = document.getElementById('coupon');
+const botonCupon = document.getElementById('calcular');
+const pCupon = document.getElementById('buttonResult');
+
+botonCupon.addEventListener('click', calcularCupon);
+
+// CON OBJETOS
+
+// const cuponObj = {
+//     'Verano': 30,
+//     'Otoño': 20,
+//     'Invierno': 10,
+//     'Primavera': 5
+// }
+
+// CON ARRAYS
+
+const couponsList = [];
+couponsList.push({
+    name: 'Verano',
+    discount: 30,
+});
+
+
+function calcularCupon() {
+
+    let discount;
+
+    const precio = Number(cuponPrice.value);
+    const coupon = cuponDiscount.value;
+
+    if(!precio || !coupon) {
+        pCupon.innerText = 'Debes llenar los recuadros primero';
+        return;
+    }
+
+    // CON OBJETOS
+
+    // if(cuponObj[coupon]) {
+    //     discount = cuponObj[coupon];
+    // } else {
+    //     pCupon.innerText = 'El cupon no es valido';
+    //     return;
+    // }
+
+
+    // CON ARRAYS
+
+    function findElement(couponElement) { //cada objeto dentro del array
+        return couponElement.name == coupon;
+    }
+
+    const couponInArray = couponsList.find(findElement);
+
+    if(couponInArray) {
+        discount = couponInArray.discount;
+    } else {
+        pCupon.innerText = "El cupon no es valido";
+        return;
+    }
+
+    const newPrice = (precio * (100 - discount)) / 100;
+
+    pCupon.innerText = 'El nuevo precio con descuento es $' + newPrice;
+
+}
